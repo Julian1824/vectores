9c4d0ac79e700d115305ee338534b54e99caf6df
Julian1824 <julianbs1813@gmail.com>
2023-11-10 22:20:24 -0500

    Entregable

--- vectores/vectores/Program.cs
+++ vectores/vectores/Program.cs
@@ -1,39 +1,63 @@
-﻿namespace vectores
+﻿using System;
+
+namespace vectores
 {
-    internal class Program
+    public class Program
     {
         static void Main(String[] args)
         {
-
-            
-            Random ramdom = new Random();
             int dimensionArray, numeroUsuario;
+            bool repetir = true;
+            String continuar;
+            Random ramdom = new Random();
 
-            Console.WriteLine("Por favor, introduce el valor que determinará la longitud del vector.");
-            dimensionArray = Convert.ToInt32(Console.ReadLine());
 
 
-            Console.WriteLine("Por favor, introduce el valor que deseas buscar dentro del vector.");
-            numeroUsuario = Convert.ToInt32(Console.ReadLine());
+            while (repetir) {
+                try
+                {   
+                Console.WriteLine("Por favor, introduce el valor que determinará la longitud del vector.");
+                dimensionArray = Convert.ToInt32(Console.ReadLine());
 
-            int[] nums = new int[dimensionArray];
-           
-            for (int i = 0; i < nums.Length; i++) {
-                nums[i] = ramdom.Next(9,18);
-                if (nums[i] == numeroUsuario)
-                {
-                    Console.WriteLine("********Ganaste********");
-                    Console.WriteLine("El numero " +  nums[i] + " se encuentra en la pocision: " + i);
-                    Console.WriteLine("************************");
+                Console.WriteLine("Por favor, introduce el valor que deseas buscar dentro del vector.");
+                numeroUsuario = Convert.ToInt32(Console.ReadLine());
+
+                    int[] nums = new int[dimensionArray];
+
+                    BuscarNumero(nums, numeroUsuario, ramdom);
                 }
-                else {
-                    Console.WriteLine("El numero no existe");
+                catch (FormatException)
+                {
+                    Console.WriteLine("Error: Has ingresado una letra en lugar de un número.");
+                }
+                
+                Console.WriteLine("¿Presiona cualquier letra para continuar o n para cerrar?");
+                continuar = Console.ReadLine().ToLower();
+
+                if (continuar == "n") {
+                    repetir = false;
                 }
             }
-
-         
-
-           
+        }
+        private static void BuscarNumero(int[] dimensionArray, int numeroUsuario, Random n)
+        {
+            bool hallar = false;
+            for (int i = 0; i < dimensionArray.Length; i++)
+            {
+                dimensionArray[i] = n.Next(1, 21);
+                if (dimensionArray[i] == numeroUsuario)
+                {
+                    Console.WriteLine("********Ganaste********");
+                    Console.WriteLine("El numero " + dimensionArray[i] + " se encuentra en la pocision: " + i);
+                    Console.WriteLine("************************");
+                    hallar = true;
+                    break;
+                }
+            }
+            if (!hallar)
+            {
+                Console.WriteLine("El numero no existe");
+            }
         }
     }
 }
